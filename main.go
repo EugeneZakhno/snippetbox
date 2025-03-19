c94f6e77d0cce44f1a1393a7e9789cdb7da727ff
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2025-03-19 23:48:08 +0300

    change port

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 	// and the servemux we just created. If http.ListenAndServe() returns an 	error
 	// we use the log.Fatal() function to log the error message and exit. 	Note
 	// that any error returned by http.ListenAndServe() is always non-nil.
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Print("Starting server on :8000")
+	err := http.ListenAndServe(":8000", mux)
 	log.Fatal(err)
 }
